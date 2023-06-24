d84038f16055c25563f6f21aeddfe28abf3d4dcb
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2023-06-25 01:16:40 +0500

    client configuration added

--- src/Payme.php
+++ src/Payme.php
@@ -2,6 +2,35 @@
 
 namespace Ittech\Payme;
 
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+use Ittech\Payme\Exceptions\PaymeException;
+use Ittech\Payme\Services\CardService;
+use Throwable;
+
 class Payme
 {
+    private PendingRequest $client;
+
+    public function __construct()
+    {
+        try {
+            $config = config('payme');
+            $proxy_url = $this->config['proxy_url'] ?? (($config['proxy_proto'] ?? '') . '://' . ($config['proxy_host'] ?? '') . ':' . ($config['proxy_port'] ?? '')) ?? '';
+            $options = is_string($proxy_url) && str_contains($proxy_url, '://') && strlen($proxy_url) > 12 ? ['proxy' => $proxy_url] : [];
+
+            $this->client = Http::log()->baseUrl($config['base_url'])->asJson()->withOptions($options);
+
+        } catch (Throwable) {
+            throw new PaymeException('Payme config not found', -1024);
+        }
+    }
+
+    public function card(): CardService
+    {
+        $client = $this->client->withHeaders([
+            'X-Auth' => config('payme.merchant_id'),
+        ]);
+        return new CardService($client);
+    }
 }
