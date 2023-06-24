1c99fc95a2d07fe874a5a6b137e043bbb341a4e0
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2023-06-25 01:16:28 +0500

    configuration key added

--- config/payme.php
+++ config/payme.php
@@ -2,5 +2,11 @@
 
 // config for Ittech/Payme
 return [
-
+    'proxy_url' => env('PAYME_PROXY_URL', ''),
+    'proxy_proto' => env('PAYME_PROXY_PROTO', ''),
+    'proxy_host' => env('PAYME_PROXY_HOST', ''),
+    'proxy_port' => env('PAYME_PROXY_PORT', ''),
+    'base_url' => env('PAYME_BASE_URL', 'https://checkout.paycom.uz'),
+    'merchant_id' => env('PAYME_MERCHANT_ID', ''),
+    'key' => env('PAYME_KEY', ''),
 ];
