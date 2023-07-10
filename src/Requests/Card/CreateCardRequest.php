83445d9407e6132108535cc11e2915781165e538
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2023-07-10 21:35:21 +0500

    removed unused codes

--- src/Requests/Card/CreateCardRequest.php
+++ src/Requests/Card/CreateCardRequest.php
@@ -4,16 +4,12 @@ namespace Ittech\Payme\Requests\Card;
 
 use Ittech\Payme\Dtos\Card;
 use Ittech\Payme\Requests\BaseRequest;
-use Spatie\LaravelData\Attributes\DataCollectionOf;
-use Spatie\LaravelData\DataCollection;
 
 class CreateCardRequest extends BaseRequest
 {
     public function __construct(
-//        #[DataCollectionOf(Card::class)]
-//        public DataCollection $card,
-        public Card           $card,
-        public bool           $save = false,
+        public Card $card,
+        public bool $save = false,
     )
     {
     }
